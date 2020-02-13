847ebdfa5a09c6c943547904eb8eca4b474dd777
Mick Groff <mick.groff@gmail.com>
2020-02-13 11:01:42 -0700

    firsttime

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.get("/", (req,res) => {
+    const bands = [{id:"1"}, {name:"pantera"}];
+    res.status(200).json(bands);
+ 
+});
+
+
+const port = 5200;
+server.listen(port, () => {
+    console.log(`\n*** Server running on http://localhost:${port} ***\n`);
+});
